7ca9511f17e7f7bb7f0f465ef169fef6a6f3455b
Ricky Kyte <137026079+rpk2301@users.noreply.github.com>
2023-07-14 18:39:28 -0400

    Add files via upload

---
+++ hammurabi/src/main/java/rocks/zipcode/Hammurabi.java
@@ -0,0 +1,24 @@
+package rocks.zipcode;
+import java.util.Random;
+import java.util.Scanner;
+public class Hammurabi
+{
+    Random rand = new Random();
+    Scanner scan = new Scanner(System.in);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
