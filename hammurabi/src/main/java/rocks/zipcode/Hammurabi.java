a5fd69bd65a59cf51c07da2f933947bd0f9eee84
ricky <rpk2301@vt.edu>
2023-07-14 20:47:57 -0400

    temp addd

--- hammurabi/src/main/java/rocks/zipcode/Hammurabi.java
+++ hammurabi/src/main/java/rocks/zipcode/Hammurabi.java
@@ -7,6 +7,12 @@ public class Hammurabi
     Scanner scan = new Scanner(System.in);
 
 
+    /*
+    Each person needs at least 20 bushels of grain per year to survive
+Each person can farm at most 10 acres of land
+It takes 2 bushels of grain to farm an acre of land
+The market price for land fluctuates yearly
+     */
 
 
 
